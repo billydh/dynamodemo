f3ca97f8aab3c0b692f6b0efd808d28a483629eb
billydh <dharmawan.billy@gmail.com>
2020-11-10 21:35:51 +1100

    migrate to dynamodb enhanced client

--- src/main/kotlin/io/codebrews/dynamodemo/Customer.kt
+++ src/main/kotlin/io/codebrews/dynamodemo/Customer.kt
@@ -1,5 +1,13 @@
 package io.codebrews.dynamodemo
 
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.*
+
 data class Customer(val emailAddress: String, val firstName: String, val lastName: String)
 
-data class CustomerPersist(val customerId: String, val emailAddress: String, val firstName: String, val lastName: String)
+@DynamoDbBean
+data class CustomerPersist(
+    @get:DynamoDbPartitionKey var customerId: String? = null,
+    var emailAddress: String? = null,
+    var firstName: String? = null,
+    var lastName: String? = null
+)
